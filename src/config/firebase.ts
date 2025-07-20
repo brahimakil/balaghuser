2cd08237e5ea60b1c2f51472c23fb97a57cbfa45
ibrahim akil <brhimakil.1@gmail.com>
2025-07-21 00:54:40 +0300

    added

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getAnalytics } from 'firebase/analytics';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCG2ZKCJDHCyXehaqOL66S7gb44o6wu7ow",
-  authDomain: "balagh-adbc4.firebaseapp.com",
-  projectId: "balagh-adbc4",
-  storageBucket: "balagh-adbc4.firebasestorage.app",
-  messagingSenderId: "849348028193",
-  appId: "1:849348028193:web:66c65700b9454efe22c060",
-  measurementId: "G-FG3Q3B40C8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
